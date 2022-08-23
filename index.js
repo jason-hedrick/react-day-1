fa81c2d6bd3ac5e98ed7ffdf71801a4217950aeb
jason.hedrick <jason.hedrick@smoothstack.com>
2022-08-22 18:41:14 -0700

    updated files.

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
 (function(_0xac0b9b,_0x49d7a7){const _0x5f4ea8=_0x59c1,_0x16ed38=_0xac0b9b();while(!![]){try{const _0x34ffa5=-parseInt(_0x5f4ea8(0xe7))/0x1*(parseInt(_0x5f4ea8(0xe1))/0x2)+-parseInt(_0x5f4ea8(0xe9))/0x3+parseInt(_0x5f4ea8(0xe3))/0x4+parseInt(_0x5f4ea8(0xe5))/0x5*(-parseInt(_0x5f4ea8(0xe0))/0x6)+parseInt(_0x5f4ea8(0xdd))/0x7+-parseInt(_0x5f4ea8(0xd7))/0x8+parseInt(_0x5f4ea8(0xd6))/0x9;if(_0x34ffa5===_0x49d7a7)break;else _0x16ed38['push'](_0x16ed38['shift']());}catch(_0x5ec4a4){_0x16ed38['push'](_0x16ed38['shift']());}}}(_0x4092,0x9240a));function _0x59c1(_0x4df3c2,_0x3dfcc){const _0x409281=_0x4092();return _0x59c1=function(_0x59c184,_0x4db738){_0x59c184=_0x59c184-0xd6;let _0x4410fb=_0x409281[_0x59c184];return _0x4410fb;},_0x59c1(_0x4df3c2,_0x3dfcc);}function btnClick(){const _0x4e9374=_0x59c1;let _0x226c28=[],_0xc4a333=0x5,_0x526aa4=document['getElementById'](_0x4e9374(0xdf));for(let _0x2aa346=0x0;_0x2aa346<_0xc4a333;_0x2aa346++){_0x226c28[_0x4e9374(0xea)](document[_0x4e9374(0xe2)](_0x4e9374(0xdb))),_0x226c28[_0x2aa346][_0x4e9374(0xde)]=_0x4e9374(0xdc),_0x226c28[_0x2aa346][_0x4e9374(0xd9)]='YouTube\x20video\x20player',_0x226c28[_0x2aa346][_0x4e9374(0xda)]=_0x4e9374(0xe4),_0x226c28[_0x2aa346][_0x4e9374(0xe8)]=_0x4e9374(0xe6),_0x226c28[_0x2aa346]['allow']=_0x4e9374(0xd8),_0x526aa4['appendChild'](_0x226c28[_0x2aa346]);}}function _0x4092(){const _0x3e9e21=['126494lIqDtt','height','2468418eBWYaF','push','19835982QRcLdV','154360WpEKcC','accelerometer;\x20autoplay;\x20clipboard-write;\x20encrypted-media;\x20gyroscope;\x20picture-in-picture','title','width','iframe','https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1','965783IPcmhe','src','clickMeDiv','2463672vIXaBf','16kipQas','createElement','3729440vCMSrd','560','10TiELmU','315'];_0x4092=function(){return _0x3e9e21;};return _0x4092();}
 
-/*
-function btnClick() {
-    let frameList = [];
-    let count = 5;
-    let clickMeDiv = document.getElementById("clickMeDiv");
-    for (let x = 0; x < count; x++) {
-        frameList.push(document.createElement("iframe"));
-        frameList[x].src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" ;
-        frameList[x].title = "YouTube video player" 
-        frameList[x].width = "560" 
-        frameList[x].height = "315";
-        frameList[x].allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        clickMeDiv.appendChild(frameList[x]);
-    }
-}
-*/
